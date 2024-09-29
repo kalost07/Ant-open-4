bb3d15970df51e5a47eb0f88afebf84817f12de0
kalost07 <k.sav4ev@gmail.com>
2024-09-29 11:15:37 +0300

    stuff

---
+++ src/Board.cpp
@@ -0,0 +1,39 @@
+#include "Board.h"
+#include "Presenter.h"
+#include "InputManager.h"
+#include "World.h"
+
+Board::Board()
+{
+
+}
+
+Board::~Board()
+{
+}
+
+void Board::init()
+{
+	m_rect = { 0,0,100,100 };
+	txt = loadTexture("board.bmp");
+	//init pulove
+}
+
+void Board::update()
+{
+	//update pulove
+}
+
+void Board::draw()
+{
+	Drawable tmp;
+	tmp.srect = m_rect;
+	tmp.texture = txt;
+	drawObject(tmp);
+	//draw pulove
+}
+
+void Board::exit()
+{
+	//quit pulove
+}
