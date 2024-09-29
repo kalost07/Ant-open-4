76a64b70166b362b9f5029d278a93ae0b1bf1a45
kalost07 <k.sav4ev@gmail.com>
2024-09-29 14:02:02 +0300

    menu

--- src/StartMenu.cpp
+++ src/StartMenu.cpp
@@ -3,37 +3,35 @@
 
 void Menu::init_quit()
 {
-	m_quit_rect.x = 1240;
-	m_quit_rect.y = 600;
-	m_quit_rect.w = 400;
-	m_quit_rect.h = 200;
+	m_quit_rect = { 840,800,400,200 };
 	m_quit_texture = loadTexture("quit.bmp");
 }
 
 void Menu::init_play()
 {
-	m_play_rect.x = 340;
-	m_play_rect.y = 600;
-	m_play_rect.w = 400;
-	m_play_rect.h = 200;
+	m_play_rect = { 840,500,400,200 };
 	m_play_texture = loadTexture("play.bmp");
+}
+void Menu::init_playBotE()
+{
+	m_playBotE_rect = { 340,500,400,200 };
+	m_playBotE_texture = loadTexture("playBotE.bmp");
+}
+void Menu::init_playBotH()
+{
+	m_playBotH_rect = { 1340,500,400,200 };
+	m_playBotH_texture = loadTexture("playBotH.bmp");
 }
 
 void Menu::init_title()
 {
-	m_title_rect.w = 1000;
-	m_title_rect.x = (1920 - m_title_rect.w) / 2;
-	m_title_rect.y = 100;
-	m_title_rect.h = 500;
+	m_title_rect = { (1920 - 1000) / 2,100,1000,500 };
 	m_title_texture = loadTexture("title.bmp");
 }
 
 void Menu::init_background_start()
 {
-	m_background_start_rect.x = 0;
-	m_background_start_rect.y = 0;
-	m_background_start_rect.w = 1920;
-	m_background_start_rect.h = 1080;
+	m_background_start_rect = { 0,0,1920,1080 };
 	m_background_start_texture = loadTexture("background.bmp");
 }
 
@@ -43,6 +41,8 @@ void Menu::init_all_start()
 	init_quit();
 	init_title();
 	init_play();
+	init_playBotE();
+	init_playBotH();
 }
 
 
@@ -61,6 +61,24 @@ void Menu::draw_play()
 	Drawable tmp;
 	tmp.texture = m_play_texture;
 	tmp.drect = m_play_rect;
+
+	tmp.srect = { 0,0,m_play_rect.w,m_play_rect.h };
+	drawObject(tmp);
+}
+void Menu::draw_playBotE()
+{
+	Drawable tmp;
+	tmp.texture = m_playBotE_texture;
+	tmp.drect = m_playBotE_rect;
+
+	tmp.srect = { 0,0,m_play_rect.w,m_play_rect.h };
+	drawObject(tmp);
+}
+void Menu::draw_playBotH()
+{
+	Drawable tmp;
+	tmp.texture = m_playBotH_texture;
+	tmp.drect = m_playBotH_rect;
 
 	tmp.srect = { 0,0,m_play_rect.w,m_play_rect.h };
 	drawObject(tmp);
@@ -90,5 +108,7 @@ void Menu::draw_all_start()
 	draw_background_start();
 	draw_quit();
 	draw_play();
+	draw_playBotE();
+	draw_playBotH();
 	draw_title();
 }
